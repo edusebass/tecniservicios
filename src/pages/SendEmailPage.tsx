9fa85400eefb77c232241863a57b276101fd0bf0
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-29 21:59:38 -0500

    add form

--- src/pages/SendEmailPage.tsx
+++ src/pages/SendEmailPage.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/NavBar";
+import SendEmailForm from "../components/Forms/SendEmailForm";
 
 const SendEmailPage = () => {
     return (
         <>
             <Navbar />
+            <SendEmailForm />
         </>
     )
 }
