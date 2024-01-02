aebf7991d7e63beea1d3e9b5ce42661101819710
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-01 20:52:27 -0500

    bub fixx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import HomePage from './pages/HomePage'
 import SendEmailPage from './pages/SendEmailPage';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
