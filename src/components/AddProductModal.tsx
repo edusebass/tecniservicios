d6f11983fb546f6bdb811546385108e3bcbb965e
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-02-06 16:33:17 -0500

    reglas listas

--- src/components/AddProductModal.tsx
+++ src/components/AddProductModal.tsx
@@ -31,31 +31,35 @@ const AddProductModal: React.FC<AddProductModalProps> = ({ visible, onCancel, on
             okButtonProps={{className: "bg-blue-600"}}
         >
             <Form form={form} layout="vertical">
-                <Form.Item label="Marca" name="marca">
+                <Form.Item label="Marca" name="marca" required>
                     <Input />
                 </Form.Item>
-                <Form.Item label="Labrado" name="labrado">
+                <Form.Item label="Labrado" name="labrado" required>
                     <Input />
                 </Form.Item>
-                <Form.Item label="Caracteristicas" name="caracteristicas">
+                <Form.Item label="Caracteristicas" name="caracteristicas" required>
                     <Input />
                 </Form.Item>
-                <Form.Item label="Ancho" name="ancho">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Alto" name="alto">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Rin" name="rin">
-                    <Input />
-                </Form.Item>
-                <Form.Item label="Costo" name="costo">
-                    <Input />
-                </Form.Item>
-                    {/* <Form.Item label="Cantidad" name="cantidad">
+                <section className='flex'>
+                    <Form.Item label="Ancho" name="ancho" rules={[{ pattern: /^\d+$/, message: 'Ingresa solo números' }]} required>
                         <Input />
-                    </Form.Item> */}
-                
+                    </Form.Item>
+                    <Form.Item label="Alto" name="alto" rules={[{ pattern: /^\d+$/, message: 'Ingresa solo números' }]} required>
+                        <Input /> 
+                    </Form.Item>
+                    
+                    <Form.Item label="Rin" name="rin" rules={[{ pattern: /^\d+$/, message: 'Ingresa solo números' }]} required className='flex flex-row'>
+                        <Input />
+                    </Form.Item>
+                </section>
+                <Form.Item label="Costo" name="costo" rules={[{ pattern: /^\d+(\.\d+)?$/, message: 'Ingresa números o números decimales con punto' }]}
+                    required
+                >
+                    <Input />
+                </Form.Item>
+                {/* <Form.Item label="Cantidad" name="cantidad">
+                    <Input />
+                </Form.Item> */}
             </Form>
         </Modal>
     );
